f0e86eb3951d912f605fe5125c41633492931a06
Vlad Apukhtin <pandalmail@gmail.com>
2016-11-19 13:45:36 +0200

    start from scratch

--- app/model/UserCredentials.ts
+++
@@ -1,8 +0,0 @@
-export class UserCredentials {
-    constructor(public Email: string,
-                public Name: string,
-                public Password: string,
-                public ConfirmPassword: string) {
-    }
-}
-let u = new UserCredentials('', '', '', '');
